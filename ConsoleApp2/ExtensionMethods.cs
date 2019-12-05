e0a4e32f062db90a15086ca7db7175bdf0960ce6
zackJKnight <zachary.j.knight@gmail.com>
2019-12-04 21:40:49 -0600

    initial commit

---
+++ ConsoleApp2/ExtensionMethods.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    public static class ExtensionMethods
+    {
+        public static string GarbleToHeck(this string s)
+        {
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            char[] alphabetUpper = alphabet.ToCharArray();
+            char[] alphabetLower = alphabet.ToLower().ToCharArray();
+            char[] garbledAlphabetUpper = new char[26];
+            char[] garbledAlphabetLower = new char[26];
+
+            string result = string.Empty;
+            int i = 0;
+            foreach (char c in alphabetUpper)
+            {
+                i = Array.IndexOf(alphabetUpper, c);
+                garbledAlphabetUpper[i] = alphabetUpper[new Random().Next(alphabetUpper.Length - 1)];
+                garbledAlphabetLower[i] = alphabetLower[new Random().Next(alphabetLower.Length - 1)];
+
+            }
+
+            foreach (char indexChar in s.ToCharArray())
+            {
+
+                var indexArray = char.IsLower(indexChar) ? alphabetLower : alphabetUpper;
+                if (indexArray.Contains(indexChar))
+                {
+                    var garbledArray = char.IsLower(indexChar) ? garbledAlphabetLower : garbledAlphabetUpper;
+                    result += garbledArray[Array.IndexOf(indexArray, indexChar)];
+                }
+                else
+                {
+                    result += indexChar;
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!";
+            Console.WriteLine(message.GarbleToHeck());
+        }
+    }
+}
